15bfda7ab2e6a1442b15ee23a2d4fce307cd3ad2
Thorsten Tüllmann <tuellmann@kit.edu>
2014-09-04 17:49:54 +0200

    moved API keys out of the repo, prepare for BFG

--- src/render.py
+++ src/render.py
@@ -4,7 +4,21 @@
 import codecs
 from argparse import ArgumentParser
 from jinja2 import Environment, FileSystemLoader
-from yaml import load
+import yaml
+import os.path
+
+
+class Loader(yaml.Loader):
+    def __init__(self, stream):
+        self._root = os.path.split(stream.name)[0]
+        super(Loader, self).__init__(stream)
+
+    def include(self, node):
+        filename = os.path.join(self._root, self.construct_scalar(node))
+        with codecs.open(filename, 'r', 'utf-8') as f:
+            return yaml.load(f, Loader)
+
+Loader.add_constructor('!include', Loader.include)
 
 
 if __name__ == "__main__":
@@ -21,4 +35,4 @@ if __name__ == "__main__":
 
 	infile = codecs.open(parsed_results.infile, 'r', 'utf-8')
 	outfile = codecs.open(parsed_results.outfile, 'w', 'utf-8')
-	outfile.write( template.render( load(infile) ) )
+	outfile.write( template.render( yaml.load(infile, Loader) ) )
